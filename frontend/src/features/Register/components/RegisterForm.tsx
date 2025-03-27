df061b7776efa6aa8643b661e5e8a6e41de4da7d
ifeanyi chimbo <ifykenchi@gmail.com>
2025-03-27 14:09:20 +0100

    BD-24: View user profile - backend

--- frontend/src/features/Register/components/RegisterForm.tsx
+++ frontend/src/features/Register/components/RegisterForm.tsx
@@ -9,113 +9,111 @@ import "react-toastify/dist/ReactToastify.css";
 import { Link, useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-		reset,
-	} = useForm<RegisterFormData>({ resolver: zodResolver(schema) });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<RegisterFormData>({ resolver: zodResolver(schema) });
 
-	const onSubmit = (data: FieldValues, e?: BaseSyntheticEvent) => {
-		e?.preventDefault();
-		axios
-			.post("http://localhost:3000/api/register", data)
-			.then(() => {
-				reset();
-				toast.success("Signup Successful!");
-			})
-			.catch((error) => {
-				toast.error(`Signup Failed. ${error.message}`);
-			});
-	};
+  const onSubmit = (data: FieldValues, e?: BaseSyntheticEvent) => {
+    e?.preventDefault();
+    axios
+      .post("http://localhost:3000/api/register", data)
+      .then(() => {
+        reset();
+        toast.success("Signup Successful!");
+      })
+      .catch((error) => {
+        toast.error(`Signup Failed. ${error.message}`);
+      });
+  };
 
-	return (
-		<>
-			<div className='large-screen'>
-				This app is unavailable for large screens. Please use a mobile device.
-			</div>
+  return (
+    <>
+      <div className="large-screen">
+        This app is unavailable for large screens. Please use a mobile device.
+      </div>
 
-			<form onSubmit={handleSubmit(onSubmit)} className='form' role='form'>
-				<p className='title'>Register</p>
-				<p className='message'>Signup now and get full access to our app.</p>
-				<div className='form-group'>
-					<label htmlFor='username' className='label'>
-						Username
-					</label>
-					<input
-						{...register("username")}
-						id='username'
-						type='text'
-						className='form-input'
-					/>
-					{errors.username && <p className='err'>{errors.username.message}</p>}
-				</div>
-				<div className='form-group'>
-					<label htmlFor='email' className='label'>
-						Email
-					</label>
-					<input
-						{...register("email")}
-						id='email'
-						type='email'
-						className='form-input'
-					/>
-					{errors.email && <p className='err'>{errors.email.message}</p>}
-				</div>
-				<div className='form-group'>
-					<label htmlFor='password' className='label'>
-						Password
-					</label>
-					<input
-						{...register("password")}
-						id='password'
-						type='password'
-						className='form-input'
-						data-testid='password'
-					/>
-					{errors.password && <p className='err'>{errors.password.message}</p>}
-				</div>
-				<div className='form-group'>
-					<label htmlFor='passwordConfirm' className='label'>
-						Confirm Password
-					</label>
-					<input
-						{...register("passwordConfirm")}
-						id='passwordConfirm'
-						type='password'
-						className='form-input'
-						data-testid='passwordConfirm'
-					/>
-					{errors.passwordConfirm && (
-						<p className='err'>{errors.passwordConfirm.message}</p>
-					)}
-				</div>
-				<Link to='/login' className='link'>
-					<button type='submit'>Submit</button>
-				</Link>
-				<p className='signin'>
-					Already have an account?{" "}
-					<span className='signup-link' onClick={() => navigate("/login")}>
-						Login
-					</span>
-				</p>
-			</form>
+      <form onSubmit={handleSubmit(onSubmit)} className="form" role="form">
+        <p className="title">Register</p>
+        <p className="message">Signup now and get full access to our app.</p>
+        <div className="form-group">
+          <label htmlFor="username" className="label">
+            Username
+          </label>
+          <input
+            {...register("username")}
+            id="username"
+            type="text"
+            className="form-input"
+          />
+          {errors.username && <p className="err">{errors.username.message}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email" className="label">
+            Email
+          </label>
+          <input
+            {...register("email")}
+            id="email"
+            type="email"
+            className="form-input"
+          />
+          {errors.email && <p className="err">{errors.email.message}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            {...register("password")}
+            id="password"
+            type="password"
+            className="form-input"
+            data-testid="password"
+          />
+          {errors.password && <p className="err">{errors.password.message}</p>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="passwordConfirm" className="label">
+            Confirm Password
+          </label>
+          <input
+            {...register("passwordConfirm")}
+            id="passwordConfirm"
+            type="password"
+            className="form-input"
+            data-testid="passwordConfirm"
+          />
+          {errors.passwordConfirm && (
+            <p className="err">{errors.passwordConfirm.message}</p>
+          )}
+        </div>
+        <button type="submit">Submit</button>
+        <p className="signin">
+          Already have an account?{" "}
+          <span className="signup-link" onClick={() => navigate("/login")}>
+            Login
+          </span>
+        </p>
+      </form>
 
-			<ToastContainer
-				position='top-right'
-				autoClose={4000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-			/>
-		</>
-	);
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
 };
 
 export default RegisterForm;
--- frontend/tests/mocks/handlers.ts
+++ frontend/tests/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-	http.post("/register", () => {
-		return HttpResponse.json("User regisered Successfully");
-	}),
+  http.post("/register", () => {
+    return HttpResponse.json("User regisered Successfully");
+  }),
 ];
