7a74ae713c7dd68865499f1905c3160afc5b84d5
ifeanyi chimbo <ifykenchi@gmail.com>
2025-03-28 20:01:03 +0100

    changes

--- frontend/src/features/Register/components/RegisterForm.tsx
+++ frontend/src/features/Register/components/RegisterForm.tsx
@@ -34,71 +34,73 @@ const RegisterForm = () => {
 			});
 	};
 
-  return (
-    <div className="form_wrapper">
-      <form onSubmit={handleSubmit(onSubmit)} className="form" role="form">
-        <p className="title">Register</p>
-        <p className="message">Signup now and get full access to our app.</p>
-        <div className="form-group">
-          <label htmlFor="username" className="label">
-            Username
-          </label>
-          <input
-            {...register("username")}
-            id="username"
-            type="text"
-            className="form-input"
-          />
-          {errors.username && <p className="err">{errors.username.message}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="email" className="label">
-            Email
-          </label>
-          <input
-            {...register("email")}
-            id="email"
-            type="email"
-            className="form-input"
-          />
-          {errors.email && <p className="err">{errors.email.message}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="password" className="label">
-            Password
-          </label>
-          <input
-            {...register("password")}
-            id="password"
-            type="password"
-            className="form-input"
-            data-testid="password"
-          />
-          {errors.password && <p className="err">{errors.password.message}</p>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="passwordConfirm" className="label">
-            Confirm Password
-          </label>
-          <input
-            {...register("passwordConfirm")}
-            id="passwordConfirm"
-            type="password"
-            className="form-input"
-            data-testid="passwordConfirm"
-          />
-          {errors.passwordConfirm && (
-            <p className="err">{errors.passwordConfirm.message}</p>
-          )}
-        </div>
-        <button type="submit" className="form_btn">Submit</button>
-        <p className="signin">
-          Already have an account?{" "}
-          <span className="signup-link" onClick={() => navigate("/login")}>
-            Login
-          </span>
-        </p>
-      </form>
+	return (
+		<div className='form_wrapper'>
+			<form onSubmit={handleSubmit(onSubmit)} className='form' role='form'>
+				<p className='title'>Register</p>
+				<p className='message'>Signup now and get full access to our app.</p>
+				<div className='form-group'>
+					<label htmlFor='username' className='label'>
+						Username
+					</label>
+					<input
+						{...register("username")}
+						id='username'
+						type='text'
+						className='form-input'
+					/>
+					{errors.username && <p className='err'>{errors.username.message}</p>}
+				</div>
+				<div className='form-group'>
+					<label htmlFor='email' className='label'>
+						Email
+					</label>
+					<input
+						{...register("email")}
+						id='email'
+						type='email'
+						className='form-input'
+					/>
+					{errors.email && <p className='err'>{errors.email.message}</p>}
+				</div>
+				<div className='form-group'>
+					<label htmlFor='password' className='label'>
+						Password
+					</label>
+					<input
+						{...register("password")}
+						id='password'
+						type='password'
+						className='form-input'
+						data-testid='password'
+					/>
+					{errors.password && <p className='err'>{errors.password.message}</p>}
+				</div>
+				<div className='form-group'>
+					<label htmlFor='passwordConfirm' className='label'>
+						Confirm Password
+					</label>
+					<input
+						{...register("passwordConfirm")}
+						id='passwordConfirm'
+						type='password'
+						className='form-input'
+						data-testid='passwordConfirm'
+					/>
+					{errors.passwordConfirm && (
+						<p className='err'>{errors.passwordConfirm.message}</p>
+					)}
+				</div>
+				<button type='submit' className='form_btn'>
+					Submit
+				</button>
+				<p className='signin'>
+					Already have an account?{" "}
+					<span className='signup-link' onClick={() => navigate("/login")}>
+						Login
+					</span>
+				</p>
+			</form>
 
 			<ToastContainer
 				position='top-right'
@@ -111,7 +113,7 @@ const RegisterForm = () => {
 				draggable
 				pauseOnHover
 			/>
-		</>
+		</div>
 	);
 };
 
